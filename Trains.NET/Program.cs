dc8263fe650a1f1f85930ac9a9f064722919f2cf
David Wengier <git@wengier.com>
2019-10-30 21:41:35 +1100

    Add project files.

---
+++ Trains.NET/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trains.NET
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using var form = new Form1();
+
+            Application.Run(form);
+        }
+    }
+}

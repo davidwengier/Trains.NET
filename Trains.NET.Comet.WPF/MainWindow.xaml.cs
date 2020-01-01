8c351114b1b625e36df03987e06a4ff375337761
David Wengier <git@wengier.com>
2020-01-01 22:36:30 +1100

    Create a basic Comet UI project

---
+++ Trains.NET.Comet.WPF/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Comet;
+using Comet.WPF;
+using Comet.WPF.Handlers;
+
+namespace Trains.NET.Comet.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            global::Comet.Skia.UI.Init();
+
+            Registrar.Handlers.Register<HStack, HStackHandler>();
+            Registrar.Handlers.Register<VStack, VStackHandler>();
+
+            MainFrame.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+            MainFrame.NavigationService.Navigate(new CometPage(MainFrame, new MainPage()));
+        }
+    }
+}
---
+++ Trains.NET.Comet/MainPage.cs
@@ -0,0 +1,24 @@
+﻿using Comet;
+using Comet.Skia;
+
+namespace Trains.NET.Comet
+{
+    public class MainPage : View
+    {
+        public MainPage()
+        {
+            this.Title("Trains.NET");
+
+            this.Body = () => new HStack()
+            {
+                new VStack()
+                {
+                    new Button("Pointer"),
+                    new Button("Track"),
+                    new Button("Eraser"),
+                },
+                new SkiaView()
+            };
+        }
+    }
+}

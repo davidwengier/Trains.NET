5debf9c7b24740ece382379413698d1f95069927
David Wengier <git@wengier.com>
2020-01-03 10:16:59 +1100

    Fix up nuget package references and platform target

--- Trains.NET.Comet.WPF/MainWindow.xaml.cs
+++ Trains.NET.Comet.WPF/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.Windows;
 using Comet;
 using Comet.WPF;
 using Comet.WPF.Handlers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Trains.NET.Comet.WPF
 {
@@ -12,6 +16,9 @@ namespace Trains.NET.Comet.WPF
     {
         public MainWindow()
         {
+            ServiceProvider serviceProvider = BuildServiceProvider();
+
+
             InitializeComponent();
 
             global::Comet.Skia.UI.Init();
@@ -20,7 +27,35 @@ namespace Trains.NET.Comet.WPF
             Registrar.Handlers.Register<VStack, VStackHandler>();
 
             MainFrame.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
-            MainFrame.NavigationService.Navigate(new CometPage(MainFrame, new MainPage()));
+            MainFrame.NavigationService.Navigate(new CometPage(MainFrame, serviceProvider.GetService<MainPage>()));
+        }
+
+        private ServiceProvider BuildServiceProvider()
+        {
+            var col = new ServiceCollection();
+            foreach (Assembly a in GetAssemblies())
+            {
+                foreach (Type t in a.GetTypes())
+                {
+                    foreach (Type inter in t.GetInterfaces())
+                    {
+                        if (inter.Namespace?.StartsWith("Trains.NET", StringComparison.OrdinalIgnoreCase) == true)
+                        {
+                            col.AddSingleton(inter, t);
+                        }
+                    }
+                }
+            }
+
+            col.AddSingleton<MainPage, MainPage>();
+
+             return col.BuildServiceProvider();
+
+            static IEnumerable<Assembly> GetAssemblies()
+            {
+                yield return typeof(Trains.NET.Engine.IGameBoard).Assembly;
+                yield return typeof(Trains.NET.Rendering.IGame).Assembly;
+            }
         }
     }
 }

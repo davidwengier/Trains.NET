63521fd0c2159226124430197c8764eb92a81844
David Wengier <git@wengier.com>
2020-03-11 22:35:19 +1100

    Minor cleanup

--- src/Trains.NET.Engine/TrainPosition.cs
+++ src/Trains.NET.Engine/TrainPosition.cs
@@ -14,23 +14,5 @@
             this.Angle = angle;
             this.Distance = distance;
         }
-
-        public void Deconstruct(out float relativeLeft, out float relativeTop, out float angle, out float distance)
-        {
-            relativeLeft = this.RelativeLeft;
-            relativeTop = this.RelativeTop;
-            angle = this.Angle;
-            distance = this.Distance;
-        }
-
-        public static implicit operator (float RelativeLeft, float RelativeTop, float Angle, float Distance)(TrainPosition value)
-        {
-            return (value.RelativeLeft, value.RelativeTop, value.Angle, value.Distance);
-        }
-
-        public static implicit operator TrainPosition((float RelativeLeft, float RelativeTop, float Angle, float Distance) value)
-        {
-            return new TrainPosition(value.RelativeLeft, value.RelativeTop, value.Angle, value.Distance);
-        }
     }
 }

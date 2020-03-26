a45b988a46351ba5d103ce9a5f6cf021151c401a
David Wengier <git@wengier.com>
2020-03-26 16:59:45 +1100

    Add tests for remaining t-intersections

--- tests/Trains.NET.Tests/TIntersectionTests.cs
+++ tests/Trains.NET.Tests/TIntersectionTests.cs
@@ -33,10 +33,36 @@ namespace Trains.NET.Tests
             GameBoard.AddTrack(2, 2);
 
             AssertTrainMovement(startAngle, startColumn, startRow, endColumn, endRow);
+        }
 
+        [Theory]
+        [InlineData(0, 1, 1, 2, 2)]
+        [InlineData(180, 3, 1, 2, 2)]
+        [InlineData(270, 2, 2, 3, 1)]
+        public void LeftRightDown_TrainMovement(float startAngle, int startColumn, int startRow, int endColumn, int endRow)
+        {
+            GameBoard.AddTrack(1, 1);
+            GameBoard.AddTrack(2, 1);
+            GameBoard.AddTrack(3, 1);
+            GameBoard.AddTrack(2, 2);
+            GameBoard.AddTrack(2, 1);
 
+            AssertTrainMovement(startAngle, startColumn, startRow, endColumn, endRow);
+        }
 
+        [Theory]
+        [InlineData(0, 1, 2, 2, 1)]
+        [InlineData(180, 3, 2, 2, 1)]
+        [InlineData(90, 2, 1, 1, 2)]
+        public void LeftRightUp_TrainMovement(float startAngle, int startColumn, int startRow, int endColumn, int endRow)
+        {
+            GameBoard.AddTrack(1, 2);
+            GameBoard.AddTrack(2, 2);
+            GameBoard.AddTrack(3, 2);
+            GameBoard.AddTrack(2, 1);
+            GameBoard.AddTrack(2, 2);
 
+            AssertTrainMovement(startAngle, startColumn, startRow, endColumn, endRow);
         }
     }
 }

1bc62a7429633a08b93f4d0cd4dbd7f92d56daf1
David Wengier <git@wengier.com>
2020-04-29 21:50:06 +1000

    Add Clone method and unique ID

--- src/Trains.NET.Engine/IMovable.cs
+++ src/Trains.NET.Engine/IMovable.cs
@@ -1,14 +1,16 @@
-﻿namespace Trains.NET.Engine
+﻿using System;
+
+namespace Trains.NET.Engine
 {
     public interface IMovable
     {
+        Guid UniqueID { get; }
         float FrontEdgeDistance { get; }
         int Column { get; }
         int Row { get; }
         float Angle { get; }
         float RelativeLeft { get; }
         float RelativeTop { get; }
-
         void SetAngle(float angle);
     }
 }

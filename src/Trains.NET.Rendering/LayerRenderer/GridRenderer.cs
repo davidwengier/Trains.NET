551d0f2061f3d12bb64bd8fca3ae00f15cd2feec
David Wengier <git@wengier.com>
2020-07-01 20:50:20 +1000

    Make sure the grid is not stuck to the top left corner

--- src/Trains.NET.Rendering/LayerRenderer/GridRenderer.cs
+++ src/Trains.NET.Rendering/LayerRenderer/GridRenderer.cs
@@ -6,16 +6,20 @@ namespace Trains.NET.Rendering
     [Order(0)]
     internal class GridRenderer : ILayerRenderer, ICachableLayerRenderer
     {
+        private bool _dirty = false;
         private readonly ITrackParameters _parameters;
+        private readonly IPixelMapper _pixelMapper;
 
-        public GridRenderer(ITrackParameters parameters)
+        public GridRenderer(ITrackParameters parameters, IPixelMapper pixelMapper)
         {
             _parameters = parameters;
+            _pixelMapper = pixelMapper;
+            _pixelMapper.ViewPortChanged += (s, e) => _dirty = true;
         }
 
         public bool Enabled { get; set; } = false;
         public string Name => "Grid";
-        public bool IsDirty => false;
+        public bool IsDirty => _dirty;
 
         public void Render(ICanvas canvas, int width, int height)
         {
@@ -26,12 +30,12 @@ namespace Trains.NET.Rendering
                 Style = PaintStyle.Stroke
             };
 
-            for (int x = 0; x < width + 1; x += _parameters.CellSize)
+            for (int x = _pixelMapper.ViewPortX; x < _pixelMapper.ViewPortWidth + 1; x += _parameters.CellSize)
             {
                 canvas.DrawLine(x, 0, x, height, grid);
             }
 
-            for (int y = 0; y < height + 1; y += _parameters.CellSize)
+            for (int y = _pixelMapper.ViewPortY; y < _pixelMapper.ViewPortHeight + 1; y += _parameters.CellSize)
             {
                 canvas.DrawLine(0, y, width, y, grid);
             }

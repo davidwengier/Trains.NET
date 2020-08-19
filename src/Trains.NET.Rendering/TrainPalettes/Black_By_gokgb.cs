676d2cf5928a1cdc791328cece167bd0f259997d
David Wengier <git@wengier.com>
2020-08-19 20:43:25 +1000

    Black as my soul, in the night, with the power out

---
+++ src/Trains.NET.Rendering/TrainPalettes/Black_By_gokgb.cs
@@ -0,0 +1,15 @@
+﻿namespace Trains.NET.Rendering.TrainPalettes
+{
+    public class Black_By_gokgb : ITrainPalette
+    {
+        public Color OutlineColor => Colors.Black;
+
+        public Color RearSectionStartColor => Colors.Black;
+
+        public Color RearSectionEndColor => Colors.Black;
+
+        public Color FrontSectionStartColor => Colors.Black;
+
+        public Color FrontSectionEndColor => Colors.Black;
+    }
+}

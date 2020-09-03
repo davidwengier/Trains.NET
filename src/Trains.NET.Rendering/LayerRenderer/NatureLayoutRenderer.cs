1e66e7000684bb44d9203efa1c4822364c560203
David Wengier <dawengie@microsoft.com>
2020-09-03 21:42:08 +1000

    Pass image cache to lower layers

--- src/Trains.NET.Rendering/LayerRenderer/NatureLayoutRenderer.cs
+++ src/Trains.NET.Rendering/LayerRenderer/NatureLayoutRenderer.cs
@@ -5,8 +5,8 @@ namespace Trains.NET.Rendering
     [Order(450)]
     public class NatureLayoutRenderer : CachableLayoutRenderer<Tree>
     {
-        public NatureLayoutRenderer(ILayout<Tree> layout, IRenderer<Tree> renderer, IImageFactory imageFactory, ITerrainMap terrainMap)
-                : base(layout, renderer, imageFactory, terrainMap)
+        public NatureLayoutRenderer(ILayout<Tree> layout, IRenderer<Tree> renderer, IImageFactory imageFactory, ITerrainMap terrainMap, IImageCache imageCache)
+                : base(layout, renderer, imageFactory, terrainMap, imageCache)
         {
             this.Name = "Nature";
             this.Enabled = true;

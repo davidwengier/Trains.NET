37e59ff166171a47706f305bec697083c633a0e9
David Wengier <git@wengier.com>
2020-10-04 12:25:08 +1100

    Move mouse handling to the button

--- src/Trains.NET.Rendering/UI/Button.cs
+++ src/Trains.NET.Rendering/UI/Button.cs
@@ -4,15 +4,16 @@ namespace Trains.NET.Rendering.UI
 {
     public class Button
     {
-        private const int TextYPadding = 15;
         private const int ButtonHeight = 20;
 
         private readonly string _label;
         private readonly Func<bool> _isActive;
         private readonly Action _onClick;
+        private bool _isHovered;
 
         public int Width { get; set; }
         public int Height { get; set; } = ButtonHeight;
+        public int PaddingX { get; set; } = 10;
 
         public Button(string label, Func<bool> isActive, Action onClick)
         {
@@ -21,27 +22,47 @@ namespace Trains.NET.Rendering.UI
             _onClick = onClick;
         }
 
-        public void Click()
-            => _onClick?.Invoke();
-
-        internal int GetMinimumWidth(ICanvas canvas)
-            => (int)canvas.MeasureText(_label, Brushes.Label);
-
-        internal void Render(ICanvas canvas, bool isHovered)
+        public bool HandleMouseAction(int x, int y, MouseAction action)
         {
+            if (x is >= 0 && x <= this.Width && y >= 0 && y <= this.Height)
+            {
+                if (action == MouseAction.Click)
+                {
+                    _onClick?.Invoke();
+                }
+                else
+                {
+                    _isHovered = true;
+                }
+                return true;
+            }
+            _isHovered = false;
+            return false;
+        }
+
+        public int GetMinimumWidth(ICanvas canvas)
+            => (int)canvas.MeasureText(_label, Brushes.Label) + (this.PaddingX * 2);
+
+        public void Render(ICanvas canvas)
+        {
+            if (this.Width == 0)
+            {
+                this.Width = GetMinimumWidth(canvas);
+            }
+
             var isActive = _isActive?.Invoke() ?? false;
 
             PaintBrush brush = isActive ? Brushes.ButtonActiveBackground : Brushes.ButtonBackground;
 
             canvas.DrawRect(0, 0, this.Width, this.Height, brush);
-            if (isHovered)
+            if (_isHovered)
             {
                 canvas.DrawRect(0, 0, this.Width, this.Height, Brushes.ButtonHoverBackground);
             }
 
             var textWidth = canvas.MeasureText(_label, Brushes.Label);
 
-            canvas.DrawText(_label, 0 + ((this.Width - textWidth) / 2), TextYPadding, Brushes.Label);
+            canvas.DrawText(_label, 0 + ((this.Width - textWidth) / 2), (float)Brushes.Label.TextSize!, Brushes.Label);
         }
     }
 }
--- src/Trains.NET.Rendering/UI/ButtonPanelBase.cs
+++ src/Trains.NET.Rendering/UI/ButtonPanelBase.cs
@@ -6,12 +6,9 @@ namespace Trains.NET.Rendering.UI
 {
     public abstract partial class ButtonPanelBase : PanelBase
     {
-        private const int TextXPadding = 10;
         private const int ButtonGap = 10;
         private const int ButtonLeft = 5;
         private int _buttonWidth = 60;
-
-        private Button? _hoverButton;
 
         protected abstract IEnumerable<Button> GetButtons();
 
@@ -22,55 +19,44 @@ namespace Trains.NET.Rendering.UI
                 x -= ButtonLeft;
                 foreach (Button button in GetButtons())
                 {
-                    if (x is >= 0 && x <= _buttonWidth && y >= 0 && y <= button.Height)
+                    if (button.HandleMouseAction(x, y, action))
                     {
-                        if (action == MouseAction.Click)
-                        {
-                            button.Click();
-                        }
-                        else
-                        {
-                            _hoverButton = button;
-                        }
                         OnChanged();
                         return true;
                     }
 
                     y -= button.Height + ButtonGap;
                 }
-                _hoverButton = null;
             }
             return false;
         }
 
-        public override void Render(ICanvas canvas, int width, int height)
+        protected override void Render(ICanvas canvas)
         {
-            var buttons = GetButtons().ToList();
+            canvas.Translate(ButtonLeft, 0);
 
+            foreach (Button button in GetButtons().ToArray())
+            {
+                button.Width = _buttonWidth;
+
+                button.Render(canvas);
+
+                canvas.Translate(0, button.Height + ButtonGap);
+            }
+        }
+
+        protected override void CalculateSize(ICanvas canvas)
+        {
             _buttonWidth = 0;
             base.InnerHeight = 0;
-            foreach (Button button in buttons)
+            foreach (Button button in GetButtons().ToArray())
             {
                 _buttonWidth = Math.Max(_buttonWidth, button.GetMinimumWidth(canvas));
                 base.InnerHeight += button.Height + ButtonGap;
             }
-            _buttonWidth += TextXPadding * 2;
 
             base.InnerWidth = _buttonWidth + 10;
             base.InnerHeight = base.InnerHeight - ButtonGap;
-
-            base.Render(canvas, width, height);
-
-            canvas.Translate(ButtonLeft, 0);
-
-            foreach (Button button in buttons)
-            {
-                button.Width = _buttonWidth;
-
-                button.Render(canvas, button == _hoverButton);
-
-                canvas.Translate(0, button.Height + ButtonGap);
-            }
         }
     }
 }

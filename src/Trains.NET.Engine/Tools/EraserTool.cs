558d3c9c27e3c0e68a4faa7d184a5722f742cec4
David Wengier <dawengie@microsoft.com>
2020-10-30 21:16:34 +1100

    Erase trains as well

--- src/Trains.NET.Engine/Tools/EraserTool.cs
+++ src/Trains.NET.Engine/Tools/EraserTool.cs
@@ -4,20 +4,32 @@
     public class EraserTool : ITool
     {
         private readonly ILayout _collection;
+        private readonly IGameBoard _gameBoard;
+        private readonly ITrainManager _trainManager;
 
         public ToolMode Mode => ToolMode.Build;
         public string Name => "Eraser";
 
-        public EraserTool(ILayout trackLayout)
+        public EraserTool(ILayout trackLayout, IGameBoard gameBoard, ITrainManager trainManager)
         {
             _collection = trackLayout;
+            _gameBoard = gameBoard;
+            _trainManager = trainManager;
         }
 
         public void Execute(int column, int row, bool isPartOfDrag)
         {
             _collection.Remove(column, row);
+            if (_gameBoard.GetMovableAt(column, row) is { } moveable)
+            {
+                _gameBoard.RemoveMovable(moveable);
+                if (_trainManager.CurrentTrain == moveable)
+                {
+                    _trainManager.CurrentTrain = null;
+                }
+            }
         }
 
-        public bool IsValid(int column, int row) => _collection.TryGet(column, row, out _);
+        public bool IsValid(int column, int row) => true;
     }
 }

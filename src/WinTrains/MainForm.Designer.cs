a065983375d024cbd05348386df9b4a991585bf7
David Wengier <git@wengier.com>
2021-11-01 15:38:31 +1100

    Add icon to Windows app

--- src/WinTrains/MainForm.Designer.cs
+++ src/WinTrains/MainForm.Designer.cs
@@ -16,17 +16,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-            ///
-            /// _skControl
-            ///
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this._skControl = new SkiaSharp.Views.Desktop.SKControl();
+            this.SuspendLayout();
+            // 
+            // _skControl
+            // 
             this._skControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this._skControl.PaintSurface += SKControl_PaintSurface;
-            this.Controls.Add(_skControl);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._skControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Trains";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

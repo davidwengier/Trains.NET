3194421b01355f263c054bf0105805713b2d2828
Scott Holden <scott@nullops.io>
2021-11-06 12:36:34 +1100

    Cleaning up moveablelayout interface.

--- src/Trains.NET.Engine/Entity/IMovableLayout.cs
+++ src/Trains.NET.Engine/Entity/IMovableLayout.cs
@@ -5,13 +5,11 @@ namespace Trains.NET.Engine;
 
 public interface IMovableLayout
 {
-    void Set(IEnumerable<IMovable> trains);
     ImmutableList<IMovable> Get();
     IEnumerable<T> Get<T>() where T : IMovable;
     IMovable? GetAt(int column, int row);
     void Add(IMovable movable);
     void Remove(IMovable movable);
-    void Clear();
     // TODO: Move this out of here!
     IEnumerable<(Track, Train, float)> LastTrackLeases { get; }
 }
